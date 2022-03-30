4658e1af04df4a19167e26f23a4a61244586e878
AT-9331 <71870002+AT-9331@users.noreply.github.com>
2022-03-30 13:38:33 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,105 @@
+configureWebpack: {
+    devtool: 'cheap-module-source-map'
+  }
+
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector("#login");
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let ques = login_text[52];
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+//[45]==P
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+console.log(ques)
+
+if (ques == 'a'){
+    let operation = login_text.slice(56 , 65);
+    let ope = operation.replace('=' , '');
+
+    var m1 = ope.match(/(\d+)/);
+    let l1 = m1[0];
+
+    var oped = ope.replace(l1,'');
+    var opee = oped.replace('+','');
+    let l2 = opee;
+
+    console.log(parseInt(l1),parseInt(l2));
+
+
+
+    var answer = parseInt(l1) + parseInt(l2);  
+    
+    console.log(ope,'.',l1,'.',l2);
+    console.log(answer);
+
+    
+    
+    
+
+}
+
+if (ques == 's'){
+    let operation = login_text.slice(61 , 70);
+    let ope = operation.replace('=' , '');
+
+    var m1 = ope.match(/(\d+)/);
+    let l1 = m1[0];
+
+    var oped = ope.replace(l1,'');
+    var opee = oped.replace('-','');
+    let l2 = opee;
+
+    console.log(parseInt(l1),parseInt(l2));
+
+
+
+    var answer = parseInt(l1) - parseInt(l2);  
+    
+    console.log(ope,'.',l1,'.',l2);
+    console.log(answer);
+
+}
+
+if (ques=='e'){
+    let ques2 = login_text[58];
+    console.log(ques2);
+
+    if (ques2=='f'){
+        let answer_=login_text.slice(70,79);        
+        var matches = answer_.match(/(\d+)/);
+        var answer = parseInt(matches[0]);
+        console.log(answer);
+
+    }
+
+    else if (ques2=='s'){
+        let answer_ = login_text.slice(76,83);
+        var matches1 = answer_.match(/(\d+)/);
+        var answer1 = matches1[0];
+
+        var answer = parseInt(matches1[0]);
+        console.log(answer);
+
+        /*
+
+        
+        var answers = answer_.replace(answer1 , '');
+        var answerss = answers.replace(',' , '');
+        
+        var matches = answerss.match(/(\d+)/);
+        var answer = parseInt(matches[0]);
+        console.log(answer);
+        */
+        
+    }
+
+    
+}
+
+
+const captcha_input_element = document.querySelector("#valuepkg3"); 
 captcha_input_element.value = answer;
+
+console.log(login_text)
+
+console.log('pp')
